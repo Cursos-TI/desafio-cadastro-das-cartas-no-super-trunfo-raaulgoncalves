6f02b3afab0af2d173ea726f318172782f8e1c4c
raaulgoncalves <luizraul2002@gmail.com>
2025-02-13 00:12:25 +0000

    CODIGO DO NIVEL NOVATO ESTA PRONTO

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,32 +1,36 @@
 #include <stdio.h>
 
-int main(){        
-    char codigo[4];          
+int main() {   
+
+    char codigo[10];  
     double populacao;     
     float area;             
     float pib;              
-    int pontosTuristicos;    
+    int pontosturisticos;    
 
-        printf("Digite a população:\n");
-        scanf("%d" , populacao);
-                    
+        printf("\nDigite o código da cidade: ");
+        scanf("%s", codigo);
 
-        return 0;
+        printf("Digite a população: ");
+        scanf("%lf", &populacao);
 
+        printf("Digite a área (em km²): ");
+        scanf("%f", &area);
 
-}
+        printf("Digite o PIB (em bilhões): ");
+        scanf("%f", &pib); 
         
-   
-                        
+        printf("Digite a quantidade de pontos turísticos: ");    
+        scanf("%d", &pontosturisticos);
 
+        printf("\n---- Dados Inseridos ----\n");
+        printf("Código da cidade: %s\n", codigo);
+        printf("População: %.0lf habitantes\n", populacao);
+        printf("Área: %.2f km²\n", area);
+        printf("PIB: %.2f bilhões\n", pib);
+        printf("Pontos turísticos: %d\n", pontosturisticos);
 
-
-
-
-
-
-
-
-
-
-
+        
+    return 0;
+    
+}
