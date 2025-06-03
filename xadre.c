5f15f622ac39f3f768af10d227d0dd05f0a08ed9
raaulgoncalves <luizraul2002@gmail.com>
2025-06-03 23:59:23 +0000

    Xadrez novato

--- xadre.c
+++ xadre.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
 
+int main() {
+    // Movimento da Torre: 5 casas para a direita
+    int movimentosTorre = 5;
+    printf("Movimento da Torre:\n");
+    for (int i = 1; i <= movimentosTorre; i++) {
+        printf("Direita\n");
+    }
+
+    printf("\n");
+
+    // Movimento do Bispo: 5 casas na diagonal para cima e à direita
+    int movimentosBispo = 5;
+    int contadorBispo = 1;
+
+    printf("Movimento do Bispo:\n");
+    while (contadorBispo <= movimentosBispo) {
+        printf("Cima Direita\n");
+        contadorBispo++;
+    }
+
+    printf("\n");
+
+    // Movimento da Rainha: 8 casas para a esquerda
+    int movimentosRainha = 8;
+    int contadorRainha = 1;
+
+    printf("Movimento da Rainha:\n");
+    do {
+        printf("Esquerda\n");
+        contadorRainha++;
+    } while (contadorRainha <= movimentosRainha);
+
+    return 0;
+}
